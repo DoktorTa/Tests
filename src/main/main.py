a30dfbcac90ffbf68a381a20f4f4dbdfee5c84ec
@DoktorTa <22112000dd@gmail.com>
2024-09-25 03:57:16 +0500

    add req

--- src/main/main.py
+++ src/main/main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 
 from src.main.stock.api.router import router as stock_router
 
+# TODO: Импорт переменных пока не работает. Указать папку
 dotenv_path = os.path.join(os.path.dirname(__file__), 'dev.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
